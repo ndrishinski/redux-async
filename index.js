b664d275e75ae4886d11eb24739261f2d79c99a7
ndrishinski <ndrishinski@yahoo.com>
2019-11-11 09:43:34 -0700

    third example

--- index.js
+++ index.js
@@ -31,3 +31,63 @@ store.dispatch(thunkedLogin()) // dispatches the thunk to the store.
 // The thunk itself (`dispatch => axios.get…`) has not yet been called.
 // When it reaches the middleware, `redux-thunk` will intercept & invoke it,
 // passing in the store's `dispatch`
+
+
+// Best Example
+// https://alligator.io/redux/redux-thunk/
+
+// component
+import { connect } from 'react-redux';
+import { addTodo } from '../actions';
+import NewTodo from '../components/NewTodo';
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddTodo: todo => {
+      dispatch(addTodo(toto));
+    }
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(NewTodo);
+
+//action creator
+export const addTodo = ({ title, userId }) => {
+    return dispatch => {
+      dispatch(addTodoStarted());
+  
+      axios
+        .post(`https://jsonplaceholder.typicode.com/todos`, {
+          title,
+          userId,
+          completed: false
+        })
+        .then(res => {
+          dispatch(addTodoSuccess(res.data));
+        })
+        .catch(err => {
+          dispatch(addTodoFailure(err.message));
+        });
+    };
+  };
+  
+  const addTodoSuccess = todo => ({
+    type: ADD_TODO_SUCCESS,
+    payload: {
+      ...todo
+    }
+  });
+  
+  const addTodoStarted = () => ({
+    type: ADD_TODO_STARTED
+  });
+  
+  const addTodoFailure = error => ({
+    type: ADD_TODO_FAILURE,
+    payload: {
+      error
+    }
+  });
